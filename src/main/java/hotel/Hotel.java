7479c7fc875324c7bfe651916cb77bfe652be31b
Clau <clau.rjimenez@gmail.com>
2022-02-10 11:48:34 +0000

    Hotel class and tests

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -1,4 +1,56 @@
 package hotel;
 
+import room.Room;
+import rooms.Bedroom;
+import rooms.ConferenceRoom;
+
+import java.util.ArrayList;
+
 public class Hotel {
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(String name) {
+        this.name = name;
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return this.bedrooms;
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return this.conferenceRooms;
+    }
+
+
+//    public void addRooms(Room... rooms) {
+//
+//        for (Room room  : rooms ) {
+//            if (room.getClass().toString() == "Bedroom") {
+//                this.bedrooms.add(room);
+//            } else {
+//                this.conferenceRooms.add(room);
+//            }
+//        }
+//
+//    }
+
+    public void addBedroom(Bedroom bedroom) {
+        this.bedrooms.add(bedroom);
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,48 @@
+import hotel.Hotel;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.Bedroom;
+import rooms.ConferenceRoom;
+import rooms.RoomType;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class HotelTest {
+
+    Bedroom singleBedroom;
+    Bedroom tripleBedroom;
+    ConferenceRoom conferenceRoom;
+//    ConferenceRoom conf2;
+    Hotel hotel;
+
+    @Before
+    public void before() {
+        singleBedroom = new Bedroom(RoomType.SINGLE, 1);
+        tripleBedroom = new Bedroom(RoomType.TRIPLE, 3);
+        conferenceRoom = new ConferenceRoom(RoomType.CONF1, "CONF 1");
+        hotel = new Hotel("Fawlty Towers");
+        hotel.addBedroom(singleBedroom);
+        hotel.addBedroom(tripleBedroom);
+        hotel.addConferenceRoom(conferenceRoom);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fawlty Towers", hotel.getName());
+    }
+
+    @Test
+    public void canAddBedroom() {
+        Bedroom doubleBedroom = new Bedroom(RoomType.DOUBLE, 2);
+        hotel.addBedroom(doubleBedroom);
+        assertTrue(hotel.getBedrooms().contains(doubleBedroom));
+    }
+
+    @Test
+    public void canAddConferenceRoom() {
+        ConferenceRoom biggerConferenceRoom = new ConferenceRoom(RoomType.CONF2, "CONF 2");
+        hotel.addConferenceRoom(biggerConferenceRoom);
+        assertTrue(hotel.getConferenceRooms().contains(biggerConferenceRoom));
+    }
 }
