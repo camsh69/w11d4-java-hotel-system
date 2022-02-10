b59e6a140ff01b4c25973c43d6cd8c2f5658b30c
Clau <clau.rjimenez@gmail.com>
2022-02-10 10:19:20 +0000

    Room class

--- src/main/java/room/Room.java
+++ src/main/java/room/Room.java
@@ -1,4 +1,31 @@
 package room;
 
+import guest.Guest;
+
+import java.util.ArrayList;
+
 public abstract class Room {
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(int capacity) {
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return this.guests;
+    }
+
+    public void setGuests(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
 }
