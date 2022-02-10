1340a317fb87dac2139701c8d32c83127c263ce0
Clau <clau.rjimenez@gmail.com>
2022-02-10 11:16:11 +0000

    Guest class and tests

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,26 @@
+import guest.Guest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before() {
+        guest = new Guest("Jane McDonald");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jane McDonald", guest.getName());
+    }
+
+    @Test
+    public void canChangeName() {
+        guest.setName("Jane MacDonald");
+        assertEquals("Jane MacDonald", guest.getName());
+    }
 }
